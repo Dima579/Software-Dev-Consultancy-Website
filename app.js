6fda653e898107e2b8f80e4c83c78858f8284f2c
Alex Dimakopoulos <47307066+Dima579@users.noreply.github.com>
2025-01-19 12:38:29 +0000

    tweaks and fixes
    - fixed hero section to not overlap navbar
    
    - changed the navbar and hero section
    
    Future updates will see the code being overhauled and new additions like sliders

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-alert("Hi! Thank you for visitng my website, please note that this website is still under progress and is having regular updates, future updates will see additional pages and more functionality.");
+alert("Hi! Thank you for visiting my website, please note that this website is still under progress and is having regular updates. Future updates will see additional pages and more functionality.");
